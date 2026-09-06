73d421a1895acfca32b51929428cff7add5caea9
U-LOTUS\cslr <tomas.ukkonen@iki.fi>
2026-09-06 15:38:07 +0300

    Mini fix.

--- src/reinforcement/RIFL_abstract2.cpp
+++ src/reinforcement/RIFL_abstract2.cpp
@@ -2216,9 +2216,10 @@ namespace whiteice
 	}
 	else if(s.size() != (numStates/STATE_HISTORY_LEN)){
 	  state = oldstate;
-	  if(firstTime) continue;
 
 	  whiteice::logging.error("ERROR: RIFL_abstact2::getState() return dimension mismatch.");
+	  
+	  if(firstTime) continue;
 	}
 	else{ // got new state
 	  state_history.push_back(s);
